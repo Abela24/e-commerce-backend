3174183b9d9a106370318c73c59e117212f18c5c
Abela24 <abelalemu95@gmail.com>
2023-02-10 20:54:13 -0500

    adding to insomina

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,13 +7,24 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
+ 
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: 'product_id',
   }
 );
 
